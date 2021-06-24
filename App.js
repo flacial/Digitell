ca16b2aff7710ff322b57bf2d6ce924c2f3024a5
BuildTools <unconfigured@null.spigotmc.org>
2021-06-24 09:34:26 +0300

    revert + dummy file

---
+++ App.js
@@ -0,0 +1,19 @@
+"use strict"; 
+import React from 'react';  
+function App() {
+    return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto"/>
+    </View>
+    );
+}
+exports["default"] = App;
+var styles = react_native_1.StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
