179b633579e320c0be1ad88a6fe157e77a25b93d
flacial <ruthemiumwithdiamond@protonmail.com>
2021-07-24 14:55:31 +0400

    Prettify

--- App.tsx
+++ App.tsx
@@ -6,48 +6,57 @@ import { Provider } from "react-redux";
 import Cwrapper from "./src/components/components-wrapper/componentsWrapper";
 import Scores from "./src/components/scores/scores";
 import { registerForPushNotificationsAsync } from "./src/utils/registerForPushNotificationsAsync";
-import * as Notifications from "expo-notifications"
-import { View, Text } from 'react-native'
-import NotificationsExp from './src/components/experimental/notificationsExp'
+import * as Notifications from "expo-notifications";
+import { View, Text } from "react-native";
+import NotificationsExp from "./src/components/experimental/notificationsExp";
 
 // Handling notifications when the app is foregrounded
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
     shouldPlaySound: true,
-    shouldSetBadge: true
-  })
-})
+    shouldSetBadge: true,
+  }),
+});
 
 const App = () => {
   const [expoPushToken, setExpoPushToken] = React.useState("");
-  const [notification, setNotification] = React.useState(false)
-  const notificationsListener = React.useRef()
-  const responseListener = React.useRef()
-
+  const [notification, setNotification] = React.useState(false);
+  const notificationsListener = React.useRef<any>();
+  const responseListener = React.useRef<any>();
 
   React.useEffect(() => {
     // Get Expo Push Token
-    registerForPushNotificationsAsync().then((token: any) => setExpoPushToken(token))
+    registerForPushNotificationsAsync().then((token: any) =>
+      setExpoPushToken(token)
+    );
 
     // Listen to the received notifications and update the state.
-    notificationsListener.current = Notifications.addNotificationReceivedListener((notification: any) => setNotification(notification))
+    notificationsListener.current =
+      Notifications.addNotificationReceivedListener((notification: any) =>
+        setNotification(notification)
+      );
 
     // Listen to user interactions with the notification
-    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => console.log(response))
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) =>
+        console.log(response)
+      );
 
     return () => {
       // Remove the listeners
-      Notifications.removeNotificationSubscription(notificationsListener.current)
-      Notifications.removeNotificationSubscription(responseListener.current)
-    }
-  }, [])
+      Notifications.removeNotificationSubscription(
+        notificationsListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
 
   return (
     <Provider store={store}>
-    {
-      // <NotificationsExp expoPushToken={expoPushToken} notification={notification}/>
-    }
+      {
+        // <NotificationsExp expoPushToken={expoPushToken} notification={notification}/>
+      }
       <Cwrapper>
         <Header />
         <Scores />
